17a0ad737d26fd0fa6fca43eb441798e1a2da77a
Gary Broderick <garybroderick82@gmail.com>
2024-09-02 10:31:53 +0000

    Test the DOM

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -6,12 +6,22 @@
 const buttonClick = require("../button")
 
 beforeEach( () => {
-    document.body.innerHTML = `<p id="par"></p>`
+    let fs = require("fs");
+
+    let fileContents = fs.readFileSync("index.html", "utf-8")
+
+    document.open();
+    document.write(fileContents);
+    document.close();
 } );
 
 describe("Dom tests", () => {
     test("expects p content to change", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You clicked");
+    });
+
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
     });
 });
